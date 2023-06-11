4f9736084bbf49d46d2ce101d3bd44cbfcf3c055
Leon Cross <leon@lscross.co.uk>
2023-06-11 14:31:08 +0100

    fix tests

--- packages/orders/jest.config.js
+++ packages/orders/jest.config.js
@@ -1,10 +1,6 @@
 module.exports = {
   roots: ['./tests'],
   testRegex: '(/tests/.*|(\\.|/)(test|spec))\\.ts',
-  // moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  // collectCoverage: true,
-  // coverageDirectory: '<rootDir>/coverage',
-  // coverageReporters: ['text', 'lcov'],
   globals: {
     'ts-jest': {
       tsconfig: './tsconfig.json',  // adjust the path if needed
